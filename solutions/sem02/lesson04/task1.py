1d6daa44ca839b542492c9fce7a7fabf370a9d84
kirill <qwerty.0045@inbox.ru>
2026-03-13 23:22:05 +0300

    HW2

--- solutions/sem02/lesson04/task1.py
+++ solutions/sem02/lesson04/task1.py
@@ -2,16 +2,53 @@ import numpy as np
 
 
 def pad_image(image: np.ndarray, pad_size: int) -> np.ndarray:
-    # ваш код
-    return image
+    if pad_size < 1:
+        raise ValueError
+    if len(image.shape) != 2:
+        padded = np.zeros(
+            (image.shape[0] + pad_size * 2, image.shape[1] + pad_size * 2, image.shape[2]),
+            dtype=image.dtype,
+        )
+        padded[pad_size:-pad_size, pad_size:-pad_size, :] = image
+    else:
+        padded = np.zeros(
+            (image.shape[0] + pad_size * 2, image.shape[1] + pad_size * 2), dtype=image.dtype
+        )
+        padded[pad_size:-pad_size, pad_size:-pad_size] = image
+
+    return padded
 
 
 def blur_image(
     image: np.ndarray,
     kernel_size: int,
 ) -> np.ndarray:
-    # ваш код
-    return image
+
+    N = image.shape[0]
+    M = image.shape[1]
+
+    if kernel_size % 2 == 0:
+        raise ValueError
+    if kernel_size < 1:
+        raise ValueError
+    if kernel_size == 1:
+        return image
+
+    blurred_image = np.zeros_like(image)
+    image = pad_image(image, kernel_size // 2)
+
+    if len(image.shape) == 3:
+        for i in range(N):
+            for j in range(M):
+                blurred_image[i, j] = np.mean(
+                    image[i : i + kernel_size, j : j + kernel_size], axis=(0, 1)
+                )
+    else:
+        for i in range(N):
+            for j in range(M):
+                blurred_image[i, j] = np.mean(image[i : i + kernel_size, j : j + kernel_size])
+
+    return blurred_image
 
 
 if __name__ == "__main__":
--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -5,6 +5,25 @@ def get_dominant_color_info(
     image: np.ndarray[np.uint8],
     threshold: int = 5,
 ) -> tuple[np.uint8, float]:
-    # ваш код
 
-    return 0, 0
+    if threshold < 1:
+        raise ValueError("threshold must be positive")
+
+    colors = [0] * 256
+
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            colors[image[i, j]] += 1
+
+    max_sum = 0
+    dominant_color = -1
+    for i in range(256):
+        if colors[i] > 0:
+            start = max(0, i - threshold + 1)
+            end = min(255, i + threshold - 1)
+            now_sum = sum(colors[start : end + 1])
+            if now_sum > max_sum:
+                max_sum = now_sum
+                dominant_color = i
+
+    return np.uint8(dominant_color), max_sum / (image.shape[0] * image.shape[1])
