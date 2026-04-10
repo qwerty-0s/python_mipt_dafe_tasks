345fb82c28dbf77756b7a8f39970f0a06c7084b7
kirill <qwerty.0045@inbox.ru>
2026-04-10 18:47:58 +0300

    HW4

--- solutions/sem02/lesson07/task1.py
+++ solutions/sem02/lesson07/task1.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -13,7 +14,66 @@ def visualize_diagrams(
     ordinates: np.ndarray,
     diagram_type: Any,
 ) -> None:
-    # ваш код
+
+    if abscissa.shape != ordinates.shape:
+        raise ShapeMismatchError
+    if diagram_type not in ("hist", "violin", "box"):
+        raise ValueError
+
+    fig = plt.figure(figsize=(10, 10), facecolor="#cfd3cd")
+
+    gs = gridspec.GridSpec(
+        2,
+        2,
+        width_ratios=[1, 4],
+        height_ratios=[4, 1],
+        hspace=0.1,
+        wspace=0.1,
+        left=0.08,
+        right=0.98,
+        top=0.98,
+        bottom=0.08,
+    )
+
+    ax_scatter = fig.add_subplot(gs[0, 1])
+    ax_diag_x = fig.add_subplot(gs[1, 1])
+    ax_diag_y = fig.add_subplot(gs[0, 0])
+    ax_diag_x.grid(True)
+    ax_diag_y.grid(True)
+    ax_scatter.grid(True)
+    ax_scatter.tick_params(labelsize=16)
+    ax_diag_x.tick_params(labelsize=16)
+    ax_diag_y.tick_params(labelsize=16)
+
+    ax_scatter.scatter(abscissa, ordinates, alpha=0.5, marker="o", s=100, color="sandybrown")
+
+    if diagram_type == "hist":
+        ax_diag_x.hist(
+            abscissa, bins=30, color="cornflowerblue", edgecolor="dimgray", alpha=0.7, density=True
+        )
+        ax_diag_y.hist(
+            ordinates,
+            bins=30,
+            orientation="horizontal",
+            color="cornflowerblue",
+            edgecolor="dimgray",
+            alpha=0.7,
+            density=True,
+        )
+
+    elif diagram_type == "violin":
+        ax_diag_x.violinplot(abscissa, vert=False)
+        ax_diag_y.violinplot(ordinates, vert=True)
+
+    elif diagram_type == "box":
+        ax_diag_x.boxplot(abscissa, vert=False)
+        ax_diag_y.boxplot(ordinates, vert=True)
+
+    ax_diag_x.invert_yaxis()
+    ax_diag_y.invert_xaxis()
+
+    plt.savefig("scatter_with_diagrams.png")
+
     pass
 
 
--- solutions/sem02/lesson07/task2.py
+++ solutions/sem02/lesson07/task2.py
@@ -1,1 +1,48 @@
-# ваш код (используйте функции или классы для решения данной задачи)
+import json
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def get_data_for_plot(file_path: str) -> tuple[np.ndarray, np.ndarray]:
+    with open(file_path, "r") as f:
+        data = json.load(f)
+    before = np.array(data["before"])
+    after = np.array(data["after"])
+    return before, after
+
+
+def sum_classes(before: np.ndarray, after: np.ndarray, classes=["I", "II", "III", "IV"]):
+    before_sum = {}
+    after_sum = {}
+    for cl in classes:
+        mask = before == cl
+        before_sum[cl] = np.sum(mask)
+        mask = after == cl
+        after_sum[cl] = np.sum(mask)
+    return before_sum, after_sum
+
+
+classes = ["I", "II", "III", "IV"]
+before, after = get_data_for_plot("data/medic_data.json")
+before_sum, after_sum = sum_classes(before, after)
+
+fig, ax = plt.subplots(figsize=(9, 6))
+x = np.arange(len(classes))
+ax.tick_params(labelsize=16)
+
+width = 0.35
+bars1 = ax.bar(x - width / 2, before_sum.values(), width, label="before", color="cornflowerblue")
+bars2 = ax.bar(x + width / 2, after_sum.values(), width, label="after", color="sandybrown")
+
+ax.set_xlabel("Mitral disease stage", fontsize=16)
+ax.set_ylabel("Amount of people", fontsize=16)
+ax.set_title("Mitral disease stages", fontsize=16)
+ax.grid(axis="y", color="#cacaca", linewidth=0.8)
+ax.set_axisbelow(True)
+ax.set_xticks(x)
+ax.set_xticklabels(classes)
+ax.legend(fontsize=16)
+
+plt.savefig("class_distribution.png")
+plt.show()
