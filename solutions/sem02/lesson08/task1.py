d2e8c00b6aa11d551d4d8085156ced3d3db9f178
kirill <qwerty.0045@inbox.ru>
2026-04-17 23:28:50 +0300

    HW5

--- solutions/sem02/lesson08/task1.py
+++ solutions/sem02/lesson08/task1.py
@@ -2,33 +2,71 @@ from functools import partial
 
 import matplotlib.pyplot as plt
 import numpy as np
-
 from IPython.display import HTML
 from matplotlib.animation import FuncAnimation
 
 
+def modulated_signal_math(modulation, fc, t):
+    if modulation is None:
+        return np.sin(2 * np.pi * fc * t)
+    else:
+        return modulation(t) * np.sin(2 * np.pi * fc * t)
+
+
+def update(frame, modulation, fc, plot_duration, animation_step, time_step, line, ax):
+    t_start = frame * animation_step
+    t_end = t_start + plot_duration
+    ax.set_xlim(t_start, t_end)
+    t = np.linspace(t_start, t_end, int(plot_duration / time_step))
+    signal = modulated_signal_math(modulation, fc, t)
+    line.set_data(t, signal)
+    return (line,)
+
+
 def create_modulation_animation(
-    modulation, 
-    fc, 
-    num_frames, 
-    plot_duration, 
-    time_step=0.001, 
-    animation_step=0.01,
-    save_path=""
+    modulation, fc, num_frames, plot_duration, time_step=0.001, animation_step=0.01, save_path=""
 ) -> FuncAnimation:
-    # ваш код
-    return FuncAnimation()
+    fig = plt.figure(figsize=(10, 6))
+    ax = plt.axes(xlim=(0, plot_duration))
+    (line,) = ax.plot([], [], lw=2)
+    ax.set_ylim(-1.5, 1.5)
+    ax.set_xlabel("Время (с)", fontsize=14)
+    ax.set_ylabel("Амплитуда", fontsize=14)
+    fig.suptitle("Модулированный сигнал", fontsize=16)
+
+    animation = FuncAnimation(
+        fig,
+        partial(
+            update,
+            modulation=modulation,
+            fc=fc,
+            plot_duration=plot_duration,
+            animation_step=animation_step,
+            time_step=time_step,
+            line=line,
+            ax=ax,
+        ),
+        frames=num_frames,
+        interval=animation_step * 1000,
+        blit=False,
+    )
+
+    if save_path:
+        animation.save(save_path, writer="pillow", fps=30)
+
+    return animation
 
 
 if __name__ == "__main__":
-    def modulation_function(t):
-        return np.cos(t * 6) 
 
-    num_frames = 100  
-    plot_duration = np.pi / 2 
-    time_step = 0.001  
-    animation_step = np.pi / 200 
-    fc = 50  
+    def modulation_function(t):
+        return np.cos(t * 6)
+
+    num_frames = 100
+    plot_duration = np.pi / 2
+    time_step = 0.001
+    animation_step = np.pi / 200
+    fc = 50
     save_path_with_modulation = "modulated_signal.gif"
 
     animation = create_modulation_animation(
@@ -38,6 +76,6 @@ if __name__ == "__main__":
         plot_duration=plot_duration,
         time_step=time_step,
         animation_step=animation_step,
-        save_path=save_path_with_modulation
+        save_path=save_path_with_modulation,
     )
     HTML(animation.to_jshtml())
--- solutions/sem02/lesson08/task2.py
+++ solutions/sem02/lesson08/task2.py
@@ -1,53 +1,143 @@
+from collections import deque
 from functools import partial
 
 import matplotlib.pyplot as plt
 import numpy as np
-
 from IPython.display import HTML
 from matplotlib.animation import FuncAnimation
 
 
+def run_wave(maze: np.ndarray, start: tuple[int, int], end: tuple[int, int]):
+
+    distances = np.full_like(maze, -1)
+    distances[start] = 0
+
+    vis_matrix = maze.copy()
+    vis_matrix[start] = 2
+
+    frames = [vis_matrix.copy()]
+    queue = deque([start])
+
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    max_x, max_y = maze.shape
+
+    while queue:
+        current_cell = queue.popleft()
+
+        if current_cell == end:
+            break
+        x, y = current_cell
+
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < max_x and 0 <= ny < max_y:
+                if maze[nx, ny] == 1 and distances[nx, ny] == -1:
+                    distances[nx, ny] = distances[x, y] + 1
+                    queue.append((nx, ny))
+                    vis_matrix[nx, ny] = 2
+                    frames.append(vis_matrix.copy())
+
+    return distances, frames, vis_matrix
+
+
+def restore_path(
+    distances: np.ndarray, start: tuple[int, int], end: tuple[int, int], final_vis: np.ndarray
+):
+
+    frames = []
+
+    if distances[end] == -1:
+        return frames
+
+    current_cell = end
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    max_x, max_y = distances.shape
+
+    vis_matrix = final_vis.copy()
+
+    while current_cell != start:
+        vis_matrix[current_cell] = 3
+        frames.append(vis_matrix.copy())
+
+        x, y = current_cell
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < max_x and 0 <= ny < max_y and distances[nx, ny] == distances[x, y] - 1:
+                vis_matrix[nx, ny] = 3
+                current_cell = (nx, ny)
+                break
+
+    vis_matrix[start] = 3
+    frames.append(vis_matrix.copy())
+
+    return frames
+
+
+def update(frame, frames, img):
+    img.set_data(frames[frame])
+    return [img]
 
 
 def animate_wave_algorithm(
-    maze: np.ndarray, 
-    start: tuple[int, int], 
-    end: tuple[int, int], 
-    save_path: str = ""
-)  -> FuncAnimation:
-    # ваш код
-    return FuncAnimation()
+    maze: np.ndarray, start: tuple[int, int], end: tuple[int, int], save_path: str = ""
+) -> FuncAnimation:
+
+    distances, wave_frames, final_wave_vis = run_wave(maze, start, end)
+    path_frames = restore_path(distances, start, end, final_wave_vis)
+
+    all_frames = wave_frames + path_frames
+    title = "Волновой алгоритм" if distances[end] != -1 else "Путь не найден!"
+
+    fig = plt.figure()
+    ax = plt.axes()
+    fig.set_size_inches(8, 8)
+    ax.set_title(title, fontsize=16)
+
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    img = ax.imshow(all_frames[0], cmap = "viridis", vmin=0, vmax=3)
+
+    animation = FuncAnimation(
+        fig,
+        partial(update, frames=all_frames, img=img),
+        frames=len(all_frames),
+        interval=200,
+        blit=True,
+    )
+
+    if save_path:
+        animation.save(save_path, writer="pillow", fps=10)
+
+    return animation
+
 
 if __name__ == "__main__":
-    # Пример 1
-    maze = np.array([
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 1, 1, 1, 1, 0],
-        [1, 1, 0, 1, 0, 1, 0],
-        [0, 0, 1, 1, 0, 1, 0],
-        [0, 0, 0, 0, 0, 1, 0],
-        [1, 1, 1, 1, 1, 1, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-    ])
+    maze = np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 1, 1, 1, 1, 0],
+            [1, 1, 0, 1, 0, 1, 0],
+            [0, 0, 1, 1, 0, 1, 0],
+            [0, 0, 0, 0, 0, 1, 0],
+            [1, 1, 1, 1, 1, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+        ]
+    )
 
     start = (2, 0)
     end = (5, 0)
-    save_path = "labyrinth.gif"  # Укажите путь для сохранения анимации
+    save_path = "labyrinth.gif"
 
     animation = animate_wave_algorithm(maze, start, end, save_path)
     HTML(animation.to_jshtml())
-    
-    # Пример 2
-    
+
     maze_path = "./data/maze.npy"
     loaded_maze = np.load(maze_path)
 
-    # можете поменять, если захотите запустить из других точек
     start = (2, 0)
     end = (5, 0)
     loaded_save_path = "loaded_labyrinth.gif"
 
     loaded_animation = animate_wave_algorithm(loaded_maze, start, end, loaded_save_path)
     HTML(loaded_animation.to_jshtml())
-    
-    
